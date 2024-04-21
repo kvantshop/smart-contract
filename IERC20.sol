b7414fe8bdcf084896f6c577fbe8632167ad5eb5
kvantshop <167026077+kvantshop@users.noreply.github.com>
2024-04-22 04:20:54 +0500

    RUBC

---
+++ IERC20.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+
+/**
+ * interface IERC20
+ */
+
+pragma solidity >=0.8.0;
+
+interface IERC20 {
+    function name() external view returns (string memory);
+    function symbol() external view returns (string memory);
+    function decimals() external view returns (uint8);
+    function totalSupply() external view returns (uint256);
+    function owner() external view returns (address);
+    function balanceOf(address account) external view returns (uint256);
+    function allowance(address owner, address spender) external view returns (uint256);
+    function transfer(address recipient, uint256 amount) external returns (bool);
+    function approve(address spender, uint256 amount) external returns (bool);
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+}
